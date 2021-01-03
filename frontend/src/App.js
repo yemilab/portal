2867d45076e98f932825554f45e7a78257b252e6
Hyounggyu Kim <code@hyounggyu.com>
2021-01-03 22:58:26 +0900

    Add grid page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { Grid, GridColumn as Column } from '@progress/kendo-react-grid';
+
+import '@progress/kendo-theme-default/dist/all.css';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid
+        style={{ height: '400px' }}
+      >
+        <Column field="ProductID" title="ID" width="40px" />
+        <Column field="ProductName" title="Name" width="250px" />
+      </Grid>
     </div>
   );
 }
