58a9a72a159be8580a5fb61ddf9b2c3b926b8e1d
Hyounggyu Kim <code@hyounggyu.com>
2021-01-11 23:04:08 +0900

    Update dashboard page

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,13 +1,22 @@
+import { ButtonGroup, Button } from '@progress/kendo-react-buttons';
+
 import { Grid, GridColumn as Column } from '@progress/kendo-react-grid';
 
 const Dashboard = () => {
   return (
-    <Grid
-      style={{ height: '400px' }}
-    >
-      <Column field="ProductID" title="ID" width="40px" />
-      <Column field="ProductName" title="Name" width="250px" />
-    </Grid>
+    <div id="Dashboard" className="dashboard-page main-content">
+      <div className="card-container grid">
+        <h3 className="card-title">Title</h3>
+        <div className="card-component">
+          <Grid
+            style={{ height: '400px' }}
+          >
+            <Column field="ProductID" title="ID" width="40px" />
+            <Column field="ProductName" title="Name" width="250px" />
+          </Grid>
+        </div>
+      </div>
+    </div>
   );
 }
 
